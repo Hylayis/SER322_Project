aa23f1e25654b63a5b38660fc4865fbf07333366
Nick Walsh <njwalsh1@asu.edu>
2018-02-22 19:54:29 -0800

    Updated index.php
    Updated index.php to more properly conform to code standards and
    cleanliness

--- index.php
+++ index.php
@@ -1,15 +1,51 @@
-<?php
-	 require 'bhconnect.php';
-	 $query = "SELECT userID, userName FROM Users";
-	 
-	 $response = @mysqli_query($dbc, $query);
-	 
-	 if($response){
-	 	echo "<html>	<title>Book Harmony</title>	<h1>Book Harmony</h1><br>	<body>	<form action=\"search.php\" method=\"post\"> <h2>Search</h2>
-	Search for a book title or Author:	<input type=\"text\" id=\"keyword\">	<input type=\"submit\" id=\"search\" value=\"search\">	<br></form>
-	<form>	<h3>Find Similar Users</h3>	Select a user: <br>	<select id=\"findusers\">";
-	 	while($row = mysqli_fetch_array($response)){
-	 		echo "<option value=\"".$row['userID']."\">".$row['userName']."</option>";
-	 	}
-	 };
-?>
+<html>
+    <title>Book Harmony</title>
+    <h1>Book Harmony</h1><br>
+    <body>
+    <form action="search.php" method="post">
+        <h2>Search</h2>
+        Search for a book title or Author:
+        <input type="text" id="keyword">
+        <input type="submit" id="search" value="search">
+        <br>
+    </form>
+    <form action="findsimilarusers.php" method="post">
+        <h3>Find Similar Users</h3>
+        Select a user: <br>
+        <select id="findusers">
+			<?php
+				 require 'bhconnect.php';
+				 $query = "SELECT userID, userName FROM Users";
+				 
+				 $response = @mysqli_query($dbc, $query);
+				 
+				 if($response){
+				 	while($row = mysqli_fetch_array($response)){
+				 		echo "<option value=\"".$row['userID']."\">".$row['userName']."</option>";
+				 	};
+				 }
+			?>
+		</select>
+		    <input type="submit" id="similarusers" name="Find Similar" value="Find Similar">
+		    </form>
+		    </body>
+		    <form action="deleteusers.php" method="post">
+		        <h3>Delete Users</h3>
+		         Select a user: <br>
+		        <select id="removeusers">
+		        <?php
+				 require 'bhconnect.php';
+				 $query = "SELECT userID, userName FROM Users";
+				 
+				 $response = @mysqli_query($dbc, $query);
+				 
+				 if($response){
+				 	while($row = mysqli_fetch_array($response)){
+				 		echo "<option value=\"".$row['userID']."\">".$row['userName']."</option>";
+				 	};
+				 }
+			?>
+			</select>
+        <input type="submit" id="deleteusers" name="Delete User" value="Delete User">
+    </form>
+</html>
