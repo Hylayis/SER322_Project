4b4f0699d43b19059dc773d328b7d7bc9989ad9b
Karen Zaunscherb <kzaunsch@asu.edu>
2018-02-24 13:43:13 +1100

    Updated to add comments
    Added comments of the INW SQL fiddling

--- findsimilarusers.php
+++ findsimilarusers.php
@@ -7,7 +7,7 @@
 <?php
   // create short variable names - needs updated with input names from HTML
 
-  $searchtype=$_POST['searchtype'];
+  $searchtype=$_POST['similarusers'];
   $searchterm=trim($_POST['searchterm']);
 
   if (!$searchtype || !$searchterm) {
@@ -28,7 +28,15 @@
   }
 
       //needs updated for SQL language to sort
-  $query = "select * from users where ".$searchtype." like '%".$searchterm."%'";
+      /*
+      *This select currently pulls all of the matching. Needs updated to show information of the users it matches with. Also needs updated in the $query field to reflect once finalized
+      SELECT T1.userID, T2.userID, T1.ISBN as matchingbook, T2.ISBN as matchingbook
+        FROM BooksRead T1, BooksRead T2
+        WHERE T1.ISBN = T2.ISBN AND T1.userID < T2.userID AND T1.userID = 1 AND T1.rating = T2.rating
+        */
+            
+
+  $query = "select distinct name from users where ".$searchtype." like '%".$searchterm."%'";
   $result = $db->query($query);
 
   $num_results = $result->num_rows;
