cfd86ad23199c5245470053688a4d9f854ee4aed
Karen Zaunscherb <kzaunsch@asu.edu>
2018-02-26 15:57:28 +1100

    Updated SQL statement
    Updated SQL statement. Needs tidying and FOR loop added

--- findsimilarusers.php
+++ findsimilarusers.php
@@ -7,16 +7,17 @@
 <?php
   // create short variable names - needs updated with input names from HTML
 
-  $searchtype=$_POST['similarusers'];
-  $searchterm=trim($_POST['searchterm']);
+  //$searchtype=$_POST['similarusers'];
+  $searchterm=trim($_POST['similarusers']);
 
+    /*
   if (!$searchtype || !$searchterm) {
      echo 'You have not entered search details.  Please go back and try again.';
      exit;
-  
+  */
 
   if (!get_magic_quotes_gpc()){
-    $searchtype = addslashes($searchtype);
+    //$searchtype = addslashes($searchtype);
     $searchterm = addslashes($searchterm);
   }
 
@@ -30,30 +31,62 @@
       //needs updated for SQL language to sort
       /*
       *This select currently pulls all of the matching. Needs updated to show information of the users it matches with. Also needs updated in the $query field to reflect once finalized
-      SELECT T1.userID, T2.userID, T1.ISBN as matchingbook, T2.ISBN as matchingbook
-        FROM BooksRead T1, BooksRead T2
-        WHERE T1.ISBN = T2.ISBN AND T1.userID < T2.userID AND T1.userID = 1 AND T1.rating = T2.rating
+      SELECT distinct u.userName, u.DoB, u.email, u.gender, count(*) as numMatches
+        FROM BooksRead T1, BooksRead T2, Users u
+        WHERE T2.userID = u.userID
+        AND T1.ISBN = T2.ISBN 
+        AND T1.userID < T2.userID 
+        AND T1.userID = 1 
+        AND T1.rating = T2.rating
+        GROUP BY userName
         */
             
 
-  $query = "select distinct name from users where ".$searchtype." like '%".$searchterm."%'";
+  $query = "SELECT distinct u.userName, u.DoB, u.email, u.gender, count(*) as numMatches
+  FROM BooksRead T1, BooksRead T2, Users u
+  WHERE T2.userID = u.userID
+        AND T1.ISBN = T2.ISBN 
+        AND T1.userID < T2.userID 
+        AND T1.userID =".$searchterm."
+        AND T1.rating = T2.rating
+        GROUP BY userName";
   $result = $db->query($query);
 
   $num_results = $result->num_rows;
+
+      /*
+  if ($num_results>0){
+        echo "<p>Number of Users found: ".$num_results."</p>";
+          //needs updated to proper table for Users 
+      for ($i=0; $i <$num_results; $i++) {
+         $row = $result->fetch_assoc();
+         echo "<p><strong>".($i+1).". Title: ";
+         echo htmlspecialchars(stripslashes($row['title']));
+         echo "</strong><br />Author: ";
+         echo stripslashes($row['author']);
+         echo "<br />ISBN: ";
+         echo stripslashes($row['isbn']);
+         echo "<br />Price: ";
+         echo stripslashes($row['price']);
+         echo "</p>";
+      }  
+  } */
 
   echo "<p>Number of Users found: ".$num_results."</p>";
 
       //needs updated to proper table for Users 
   for ($i=0; $i <$num_results; $i++) {
      $row = $result->fetch_assoc();
-     echo "<p><strong>".($i+1).". Title: ";
-     echo htmlspecialchars(stripslashes($row['title']));
-     echo "</strong><br />Author: ";
-     echo stripslashes($row['author']);
-     echo "<br />ISBN: ";
-     echo stripslashes($row['isbn']);
-     echo "<br />Price: ";
-     echo stripslashes($row['price']);
+     echo "<p><strong>".($i+1).". User Name: ";
+     echo htmlspecialchars(stripslashes($row['username']));
+     echo "</strong><br />Date of Birth: ";
+     echo stripslashes($row['DoB']);
+     echo "<br />Email: ";
+     echo stripslashes($row['email']);
+     echo "<br />Gender: ";
+     echo stripslashes($row['gender']);
+     echo "<br />Number of Books Matched: ";
+     echo stripslashes($row['numMatches']);
      echo "</p>";
   }
 
