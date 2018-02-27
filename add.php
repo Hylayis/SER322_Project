f65ab161f8348714ffebb1c3f4f912f75363a140
Karen Zaunscherb <kzaunsch@asu.edu>
2018-02-27 14:33:44 +1100

    Allows addition from books.xml

--- add.php
+++ add.php
@@ -1,8 +1,6 @@
 <?php
 //initial setup 
 $xmlDoc = new DOMDocument();
-$xmlDoc->load("authors.xml");
-
 $con = mysqli_connect("localhost","bhweb","supersecure","bookharmony");
 
 // Check connection
@@ -11,15 +9,16 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-/*
-mysqli_select_db($mysql_database, $bd) 
-    or die("Oops some thing went wrong");
-*/
 
+/*
+This next section adds AUTHORS to the database
+*/
+$xmlDoc->load("authors.xml");
+//Reads XML file
 $xmlObject = $xmlDoc->getElementsByTagName('item');
 $itemCount = $xmlObject->length;
 echo $itemCount;
-
+//iterates through, adding each element to mysql table
 for ($i=0; $i < $itemCount; $i++){
   $authorID = $xmlObject->item($i)->getElementsByTagName('authorID')->item(0)->childNodes->item(0)->nodeValue;
   $name  = $xmlObject->item($i)->getElementsByTagName('name')->item(0)->childNodes->item(0)->nodeValue;
@@ -27,12 +26,37 @@ for ($i=0; $i < $itemCount; $i++){
   $sql   = "INSERT INTO authors (authorID, name, gender) VALUES ('$authorID', '$name', '$gender');";
   
     if (mysqli_query($con, $sql)) {
-        echo "New record created successfully";
+        echo "New record created successfully".$authorID."<br>";
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($con);
     }
 }
 
+/*
+This next section adds BOOKS to the database
+*/
+$xmlDocBooks = new DOMDocument();
+$xmlDocBooks->load("books.xml");
+//Reads XML file
+$xmlObject = $xmlDocBooks->getElementsByTagName('item');
+$itemCount = $xmlObject->length;
+echo $itemCount;
+//iterates through, adding each element to mysql table
+for ($i=0; $i < $itemCount; $i++){
+  $ISBN = $xmlObject->item($i)->getElementsByTagName('ISBN')->item(0)->childNodes->item(0)->nodeValue;
+  $title  = $xmlObject->item($i)->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
+  $author  = $xmlObject->item($i)->getElementsByTagName('author')->item(0)->childNodes->item(0)->nodeValue;
+  $pages  = $xmlObject->item($i)->getElementsByTagName('pages')->item(0)->childNodes->item(0)->nodeValue;
+  $type  = $xmlObject->item($i)->getElementsByTagName('type')->item(0)->childNodes->item(0)->nodeValue;
+  $genre  = $xmlObject->item($i)->getElementsByTagName('genre')->item(0)->childNodes->item(0)->nodeValue;
+  $sql   = "INSERT INTO books (ISBN, title, author, pages, type, genre) VALUES ('$ISBN', '$title', '$author',  '$pages', '$type' ,'$genre');";
+  
+    if (mysqli_query($con, $sql)) {
+        echo "New record created successfully:".$ISBN."<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($con);
+    }
+}
 
 
     mysqli_close($con);
